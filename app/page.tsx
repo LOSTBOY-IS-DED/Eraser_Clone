70e3fb087bae7db2bde07b1203f2b4639bfcae4e
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-11-19 01:40:20 +0530

    Project setup done

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hi there you are welcome</h1>
+      <Button>Welcome sir</Button>
+    </div>
+  );
+}
