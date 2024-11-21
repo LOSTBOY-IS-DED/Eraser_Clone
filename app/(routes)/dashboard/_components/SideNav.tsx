c5bfe2b18f45024ea1862bbe060ed43f380728aa
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-11-21 23:00:00 +0530

    side bar completed

--- app/(routes)/dashboard/_components/SideNav.tsx
+++ app/(routes)/dashboard/_components/SideNav.tsx
@@ -2,13 +2,20 @@ import React from "react";
 
 import SideNavTopSection from "./SideNavTopSection";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import SideNavBottomSection from "./SideNavBottomSection";
+import { Archive, Flag, Github } from "lucide-react";
 
 function SideNav() {
   const { user }: any = useKindeBrowserClient();
 
   return (
-    <div className=" h-screen fixed w-72 border-r border-[1px] p-6">
-      <SideNavTopSection user={user} />
+    <div className=" h-screen fixed w-72 border-r border-[1px] p-6 flex flex-col">
+      <div className="flex-1">
+        <SideNavTopSection user={user} />
+      </div>
+      <div>
+        <SideNavBottomSection />
+      </div>
     </div>
   );
 }
--- app/(routes)/dashboard/_components/SideNavBottomSection.tsx
+++ app/(routes)/dashboard/_components/SideNavBottomSection.tsx
@@ -1,7 +1,88 @@
+import { Button } from "@/components/ui/button";
+import { Archive, Flag, Github } from "lucide-react";
 import React from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 
 function SideNavBottomSection() {
-  return <div>sideNavBottomSection</div>;
+  const menuList = [
+    {
+      id: 1,
+      name: "Getting Started",
+      icon: Flag,
+      path: "",
+    },
+    {
+      id: 2,
+      name: "Github",
+      icon: Github,
+      path: "",
+    },
+    {
+      id: 3,
+      name: "Archive",
+      icon: Archive,
+      path: "",
+    },
+  ];
+  return (
+    <div>
+      {menuList.map((menu, index) => (
+        <h2 className="flex gap-[0.25rem] p-2 px-2 text-[14px] hover:bg-gray-100 rounded-md cursor-pointer">
+          <menu.icon className="h-5 w-5" />
+          {menu.name}
+        </h2>
+      ))}
+
+      {/* Add New File Button */}
+
+      <Dialog>
+        <DialogTrigger className="w-full">
+          <Button className="w-full bg-blue-600 hover:bg-blue-700 justify-start mt-3">
+            New File
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create New File</DialogTitle>
+            <DialogDescription>
+              <Input className="mt-3" placeholder="Write file Name" />
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button type="button" className="bg-blue-600 hover:bg-blue-700">
+                Create
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+
+      {/* progress bar */}
+
+      <div className="h-4 w-full bg-gray-200 rounded-full mt-5">
+        <div className="h-4 w-[40%] bg-blue-600 rounded-full"></div>
+      </div>
+
+      <h2 className="text-[12px] mt-3 ">
+        {" "}
+        <strong>1</strong> out of <strong>5</strong> files used
+      </h2>
+      <h2 className="text-[12px] mt-1">
+        Upgrade you plan for unlimited access
+      </h2>
+    </div>
+  );
 }
 
 export default SideNavBottomSection;
